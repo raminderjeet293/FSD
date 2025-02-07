251e72a61702253eb554ed5de4ed6c48248e88e5
raminderjeet293 <raminderjeetkaur02@gmail.com>
2025-02-07 09:41:20 +0530

    button component created

--- react/r_app/src/index.js
+++ react/r_app/src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Btn } from "./Components/Button";
+// import App from './App';Y
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-const nm="MCA Sec A"
-const r_e=['props','element','states','components']
-const ele=React.createElement('h1',{},'hello'+r_e+nm)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Btn />
+  </React.StrictMode>
+);
+// const nm = "MCA Sec A";
+// const r_e = ["props", "element", "states", "components"];
+// const ele = React.createElement("h1", {}, "hello" + r_e + nm);
 
-root.render(ele)
+// root.render(ele);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
